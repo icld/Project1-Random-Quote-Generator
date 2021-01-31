147ee243e03ad7043a4fc96ccf32dea693d1781c
icld <76025734+icld@users.noreply.github.com>
2021-01-30 18:59:55 -0800

    Working program, added transitions and color change

--- js/array-quotes.js
+++ js/array-quotes.js
@@ -1,33 +1,17 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+const quotes = [
+  { quote: 'I am a quote 0', source: 'source 0' },
+  { quote: 'I am a quote 1', source: 'source 1' },
+  { quote: 'I am a quote 2', source: 'source 2' },
+  { quote: 'I am a quote 3', source: 'source 3', citation: 'citation 3' },
+  { quote: 'I am a quote 4', source: 'source 4', citation: 'citation 4' },
+  { quote: 'I am a quote 5', source: 'source 5', citation: 'citation 5' },
+  { quote: 'I am a quote 6', source: 'source 6', citation: 'citation 6' },
+  { quote: 'I am a quote 7', source: 'source 7', citation: 'citation 7', year: 'year 7' },
+  { quote: 'I am a quote 8', source: 'source 8', citation: 'citation 8', year: 'year 8' },
+  { quote: 'I am a quote 9', source: 'source 9', citation: 'citation 9', year: 'year 9' },
+  { quote: 'I am a quote 10', source: 'source 10', year: 'year 10' },
+  { quote: 'I am a quote 11', source: 'source 11', year: 'year 11' },
+  { quote: 'I am a quote 12', source: 'source 12', year: 'year 12' },
+];
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
-
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log(quotes.length)
--- js/script.js
+++ js/script.js
@@ -3,31 +3,42 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+function getRandomNumber(max) {
+  let randomNumber = Math.floor(Math.random() * Math.floor(max));
+  return randomNumber
+}
+function getRandomQuote() {
+  let i = Math.floor(Math.random() * Math.floor(quotes.length));
+  let randomQuote = quotes[i];
+  return randomQuote
+}
 
-/*** 
- * `quotes` array 
-***/
+function getRandomRGB() {
+  let randomColor = `rgb(${getRandomNumber(256)},${getRandomNumber(256)},${getRandomNumber(256)})`;
+  document.body.style.background = randomColor;
+  document.body.style.transition = `1s`;
 
+}
 
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let html = `<p class ="quote" >${randomQuote.quote}</p> <p class="source">${randomQuote.source}`;
+  if (randomQuote.citation) {
+    html += `<span class="citation">${randomQuote.citation}</span>`;
 
-/***
- * `getRandomQuote` function
-***/
+  } if (randomQuote.year) {
+    html += `<span class="year">${randomQuote.year}</span>`;
+  }
+  html += `</p>`;
+  getRandomRGB();
+  console.log(randomQuote, randomQuote.citation, html);
+  document.getElementById('quote-box').innerHTML = html;
+};
 
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
+setInterval(printQuote, 10000)
 
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+// runs the function printQuote upon opening loading the page.  Found this approach on mdn while searching for a way to run function on loading a page
+window.onload = printQuote();
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
